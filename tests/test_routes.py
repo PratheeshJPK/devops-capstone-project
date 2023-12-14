a73ee4aed788105ee68a93c2dfc11afaf1967122
PratheeshJPK <pratheeshjpk@gmail.com>
2023-12-14 01:21:48 -0500

    Implementation and testcases for deleteaccount

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -188,4 +188,10 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_delete_an_account(self):
+        """it should test delete an account from the service """
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        
     
